9e14d383329131ecaa1e438b6511622693969762
Asritha <chikkalaasritha@gmail.com>
2025-06-03 22:16:50 +0530

    removed capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.codeecho.app',
-  appName: 'Code Echo',
-  webDir: 'build'
-};
-
-export default config;
