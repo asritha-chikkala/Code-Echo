438eba4220adb05a3d33eced1d607e1b6c3491bc
Asritha <chikkalaasritha@gmail.com>
2025-06-03 21:11:45 +0530

    Add Capacitor native Android project files

---
+++ android/app/src/main/java/com/codeecho/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.codeecho.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.codeecho.app',
+  appName: 'Code Echo',
+  webDir: 'build'
+};
+
+export default config;
